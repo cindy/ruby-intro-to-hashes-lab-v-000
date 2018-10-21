0f9dd42b7f16d4731d47f3114be844301c99f62d
Cindy Pallares <cindy.pallaresq@gmail.com>
2018-10-21 00:46:01 +0000

    automatically backed up by learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -38,7 +38,7 @@ def monopoly_with_fourth_tier
 		railroads:
 		{
 		pieces:4,
-		names: {reading_railroad: {}, pennsylvania_railroad: {}, b_and_o_railroad:{}, shortline:{}},
+		names: {reading_railroad: {mortgage_value: true}, pennsylvania_railroad: {}, b_and_o_railroad:{}, shortline:{}},
 		rent_in_dollars:
 				{
 				one_piece_owned:25,
